ef5e9af6aa5d04e8c95db7b1c7cbd2d3be635530
Dmitry <volcharo@gmail.com>
2024-06-04 16:06:10 +0300

    code

--- components/header.php
+++ components/header.php
@@ -11,8 +11,25 @@
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ffffff">
     <title>Валдайский мастер</title>
+    
+    <!-- Bootstrap -->
+    <link href="./assets/css/custom.min.css" rel="stylesheet">
+    <script src="./assets/js/bootstrap.bundle.js"></script>
+    <!-- /Bootstrap -->
+
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <!-- /jQuery -->
+
+    <!-- Lightbox -->
+    <link href="./assets/lightbox/css/lightbox.css" rel="stylesheet" />
+    <script src="./assets/lightbox/js/lightbox.js"></script>
+    <!-- /Lightbox -->
+
+    <!-- Slick slider -->
+    <link href="./assets/slick/slick.css" rel="stylesheet" type="text/css" />
+    <script type="text/javascript" src="./assets/slick/slick.min.js"></script>
+    <!-- /Slick slider -->
+
 </head>
 <body>
-    
-</body>
-</html>
