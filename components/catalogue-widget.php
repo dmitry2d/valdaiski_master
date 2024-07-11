955d53d255ddf32ba0bf79d7fee619d98a6a2c4d
Dmitry <volcharo@gmail.com>
2024-07-11 15:03:24 +0300

    code

--- components/catalogue-widget.php
+++ components/catalogue-widget.php
@@ -2,7 +2,10 @@
 
 <div class="catalogue-widget my-5">
     <div class="container">
-        <div class="title d-md-none my-4">Каталог проектов</div>
+        <div class="d-flex align-items-end my-4">
+            <div class="title">Каталог проектов</div>
+            <div class="flex-grow-1 border-top border-style-light mb-1 d-none d-xl-block"></div>
+        </div>
         <div class="items row g-3">
             <?php include "./components/catalogue-widget-item.php" ?>
             <?php include "./components/catalogue-widget-item.php" ?>
--- page-works.php
+++ page-works.php
@@ -8,11 +8,38 @@
 ?>
 
 <div class="page-content">
-
-    <div class="container">
-
-        <div class="works-gallery works-gallery-full">
     
+    <div class="works-gallery works-gallery-full">
+        
+        <div class="full-photo d-lg-none mb-5">
+            <img src="./assets/img/photo/house01.png" alt="" class="w-100">
+        </div>
+
+        <div class="container">
+
+            <div class="full-icon d-none d-lg-block text-center">
+                <img src="./assets/img/tool-green.svg" alt="">
+            </div>
+
+
+            <div class="full-title text-center my-3">Наши работы одноэтажных каркасных домов в Санкт-Петербурге и Москве</div>
+            
+            <div class="full-description text-center my-5">В данном разделе вы найдете фото реализованных каркасных проектов, готовых домов и бань, построенных нашими мастерами. Мы строим исключительно одноэтажные каркасные дома из нашего каталога в скандинавском и финском стиле из строганного и сухого материала высшего сорта. А так же реализуем индивидуальные проекты, разработанные под вас нашими архитекторами.</div>    
+
+            <div class="full-tags d-flex flex-wrap justify-content-center align-items-center my-5">
+                <div class="full-tag active">Все работы</div>
+                <div class="full-tag">Сканди Смарт</div>
+                <div class="full-tag">Финские и Скандинавские</div>
+                <div class="full-tag">Сканди Смарт</div>
+                <div class="full-tag">Финские и Скандинавские</div>
+                <div class="full-tag">Сканди Смарт</div>
+                <div class="full-tag">Финские и Скандинавские</div>
+                <div class="full-tag">Сканди Смарт</div>
+                <div class="full-tag">Финские и Скандинавские</div>
+                <div class="full-tag">Сканди Смарт</div>
+                <div class="full-tag">Финские и Скандинавские</div>
+            </div>
+
             <div class="row align-items-stretch">
 
                 <div class="col-lg-6 col-xl-4 d-flex"><?php include './components/works-gallery-item-social.php' ?></div>
@@ -27,6 +54,12 @@
             </div>
     
         </div>
+
+        <!-- Навигация страниц -->
+        <div class="my-3 pt-2 d-flex justify-content-center">
+            <?php include "./components/page-nav.php" ?>
+        </div>
+        <!-- / Навигация страниц -->
 
     </div>
 
