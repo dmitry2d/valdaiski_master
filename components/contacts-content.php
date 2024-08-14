d21ce0e89f56143412f4b59fa0cff970218f802e
Dmitry <volcharo@gmail.com>
2024-08-14 13:17:33 +0300

    code

--- components/contacts-content.php
+++ components/contacts-content.php
@@ -151,22 +151,165 @@
     <!-- / Координаты -->
 
     
-    <!-- Баннеры -->
-     <div class="banner">
-        <div class="data d-flex">
-            <div class="col-4">
-                <img src="./assets/img/photo/ustugna.png" alt="">
-            </div>
-                                    
-            <div class="title"></div>
-        </div>
-        <div class="gallery"></div>
-        <div class="contact">
-        
-        </div>
-     </div>
-    <!-- / Баннеры -->
+    <!-- Баннер -->
 
+    <div class="contact-geo contact-banner my-5">
+
+        <div class="banner-description order-1">
+
+            <div class="top h-100">
+                <div class="d-flex h-100 flex-column flex-md-row">
+                    <div class="col-lg-4">
+                        <div class="map h-100">
+                            <img src="./assets/img/photo/ustugna.png" alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-8 ps-md-4 order-1 order-md-2">
+
+                        <div class="geo-title mt-2 mb-3">
+                            Производство в Устюжне
+                        </div>
+                        <div class="fw-bold">
+                            Адрес:
+                        </div>
+                        <div class="geo-address">
+                            Вологодская область, г. Устюжна, производственная база <b>«Валдайский мастер»</b>
+                        </div>
+                        <div class="my-2">
+                            <b>Время работы:</b> ежедневно с 09:00 до 19:00
+                        </div>
+                        <div class="mb-4">
+                            <i>Экскурсия на производство по предварительной записи.</i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+                
+        <div class="banner-contacts order-3">
+
+            <div class="d-flex flex-column h-100 w-100 justify-content-end">
+                <div class="border-1 border-top border-style-light-grey my-4 opacity-50"></div>
+                <div class="bottom d-flex flex-column flex-md-row">
+                    <div class="bottom-left flex-grow-1">
+                        <div class="phone">+7 (800) 550-96-76</div>
+                        Звонок по России бесплатный
+                    </div>
+                    <div class="bottom-right mt-2 mt-md-0">
+                        Электронная почта:<br>
+                        <a href="mailto:sale@domavn.ru">sale@domavn.ru</a>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
+        <div class="banner-gallery ps-lg-5 pt-4 pt-lg-0 order-2">
+            <!-- Слайдер - Галерея -->
+            <div class="slider-gallery">
+                <div class="slider-gallery-items">
+                    <div>
+                        <div class="wrapper"><img src="./assets/img/photo/house01.png" alt=""></div>
+                    </div>
+                    <div>
+                        <div class="wrapper"><img src="./assets/img/photo/house01.png" alt=""></div>
+                    </div>
+                    <div>
+                        <div class="wrapper"><img src="./assets/img/photo/article_house_1.jpg" alt=""></div>
+                    </div>
+                    <div>
+                        <div class="wrapper"><img src="./assets/img/photo/article_house_1.jpg" alt=""></div>
+                    </div>
+                </div>
+                <div class="slider-gallery-nav pt-2"></div>
+            </div>
+            <!-- / Слайдер - Галерея -->
+        </div>
+
+
+    </div>
+
+    <!-- / Баннер -->
+    
+    <!-- Баннер -->
+
+    <div class="contact-geo contact-banner my-5">
+
+<div class="banner-description order-1">
+
+    <div class="top h-100">
+        <div class="d-flex h-100 flex-column flex-md-row">
+            <div class="">
+
+                <div class="geo-title mt-2 mb-3">
+                    Приглашаем на построеные и строящиеся объекты в Москве и Ленинградской области
+                </div>
+                <div class="theme-button gradient outline padding-2 d-block d-md-inline-block mb-4 mb-md-5">
+                    <a href="">
+                        <div class="wrap bg-style-lightest text-center">
+                            Записаться на экскурсию
+                        </div>
+                    </a>
+                </div>
+                <div class="my-2">
+                    <b>Время работы:</b> ежедневно с 09:00 до 19:00
+                </div>
+                <div class="mb-4">
+                    <i>Посещение объектов строительсва по предварительной записи.</i>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+        
+<div class="banner-contacts order-3">
+
+    <div class="d-flex flex-column h-100 w-100 justify-content-end">
+        <div class="border-1 border-top border-style-light-grey my-4 opacity-50"></div>
+        <div class="bottom d-flex flex-column flex-md-row">
+            <div class="bottom-left flex-grow-1">
+                <div class="phone">+7 (800) 550-96-76</div>
+                Звонок по России бесплатный
+            </div>
+            <div class="bottom-right mt-2 mt-md-0">
+                Электронная почта:<br>
+                <a href="mailto:sale@domavn.ru">sale@domavn.ru</a>
+            </div>
+        </div>
+    </div>
+
+
+</div>
+
+<div class="banner-gallery ps-lg-5 pt-4 pt-lg-0 order-2">
+    <!-- Слайдер - Галерея -->
+    <div class="slider-gallery">
+        <div class="slider-gallery-items">
+            <div>
+                <div class="wrapper"><img src="./assets/img/photo/house01.png" alt=""></div>
+            </div>
+            <div>
+                <div class="wrapper"><img src="./assets/img/photo/house01.png" alt=""></div>
+            </div>
+            <div>
+                <div class="wrapper"><img src="./assets/img/photo/article_house_1.jpg" alt=""></div>
+            </div>
+            <div>
+                <div class="wrapper"><img src="./assets/img/photo/article_house_1.jpg" alt=""></div>
+            </div>
+        </div>
+        <div class="slider-gallery-nav pt-2"></div>
+    </div>
+    <!-- / Слайдер - Галерея -->
+</div>
+
+
+</div>
+
+<!-- / Баннер -->
 
 </div>
 
